76390c143f0f91c71f318b6da88656f13d4b9b6c
Siddhesh <64964929+siddheshsule@users.noreply.github.com>
2022-03-25 23:06:07 +0100

    Create BinarySearch.py

---
+++ 01_DSA_Python/02_Algorithms/BinarySearch.py
@@ -0,0 +1,24 @@
+# Binary Search Algorithm
+
+def binary_search(myList,target):
+    low = 0
+    high = len(myList) - 1
+    
+    while(low <= high):
+        mid = int((low + high) / 2)
+        if (myList[mid] == target):
+            return mid
+        elif myList[mid] > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+# Testing the Algorithm
+lst1 = [4,5,6,7,8,9,10,11,12,13]
+guess = 15
+
+print(binary_search(lst1, guess))           
+            
+
+    
